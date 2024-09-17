7e29b88b2b2b12b0a85620d9d5afec4ef5bfde14
Alfer <89336044+ralfer-99@users.noreply.github.com>
2024-09-17 20:23:41 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,12 @@
-
-import Layout from './src/components/Layout'
+import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
-import profilePic from './public/images/profile/alf.png'
-import AnimatedText from './src/components/AnimatedText'
-import { LinkArrow } from './src/components/Icons'
-import HireMe from './src/components/HireMe'
-import lightBulb from './public/images/svgs/miscellaneous_icons_1.svg'
-import TransitionEffect from './src/components/TransitionEffect'
+import profilePic from '../../public/images/profile/alf.png'
+import AnimatedText from '@/components/AnimatedText'
+import { LinkArrow } from '@/components/Icons'
+import HireMe from '@/components/HireMe'
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
+import TransitionEffect from '@/components/TransitionEffect'
 
 export default function Home() {
   return (
