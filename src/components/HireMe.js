c6131a99279d7477683b78f204e213a07e819c0b
Alfer <89336044+ralfer-99@users.noreply.github.com>
2024-09-20 18:06:03 +0530

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 const HireMe = () => {
   return (
-    <div className='w-48 h-48 fixed left-10 bottom-10 z-20 flex items-center justify-center'>
-    <div className='w-full h-full relative flex items-center justify-center overflow-hidden'>
-      <Image className="circular-text w-full h-full z-20 fill-dark animate-spin-slow" src="https://ralfer-99.github.io/circular-text.png"
- alt='some texts'  width={800} height={800} />
+    <div className='w-48 h-48 fixed left-10 bottom-10 z-20 flex items-center justify-center md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0
+    '>
+    <div className='w-full h-full relative flex items-center justify-center overflow-hidden md:w-25'>
+      <Image className="circular-text w-full h-full z-20 fill-dark animate-spin-slow" src="/circular-text.png" alt='some texts' width={800} height={800} />
 
       <a href='mailto:ammralfer@gmail.com' className='flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         bg-dark text-light shadow-md border border-solid
@@ -16,6 +16,7 @@ const HireMe = () => {
 
          dark:bg-light dark:text-dark hover:dark hover:dark:text-light
                   hover:dark:border-light
+                  md:w-25 md:h-25 md:text-[15px]
         
         '>
         Hire Me
