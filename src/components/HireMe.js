f31f69e11acd38fb3b2c7cd64e3481173a53daf5
Alfer <89336044+ralfer-99@users.noreply.github.com>
2024-09-20 18:33:50 +0530

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -7,7 +7,7 @@ const HireMe = () => {
     <div className='w-48 h-48 fixed left-10 bottom-10 z-20 flex items-center justify-center md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0
     '>
     <div className='w-full h-full relative flex items-center justify-center overflow-hidden md:w-25'>
-      <Image className="circular-text w-full h-full z-20 fill-dark animate-spin-slow" src="/circular-text.png" alt='some texts' width={800} height={800} />
+      <Image className="circular-text w-full h-full z-20 fill-dark animate-spin-slow" src="../../public/circular-text.png" alt='some texts' width={800} height={800} />
 
       <a href='mailto:ammralfer@gmail.com' className='flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         bg-dark text-light shadow-md border border-solid
