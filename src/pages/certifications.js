766255733e7b2d095e8f7ca5d4402954af658666
Alfer <89336044+ralfer-99@users.noreply.github.com>
2024-09-20 18:02:06 +0530

    Update certifications.js

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -51,7 +51,7 @@ const MovingImg = ({ title, img, link }) => {
       onMouseMove={handleMouse}
       onMouseLeave={handleMouseLeave}
     >
-      <h2 className='capitalize text-xl font-semibold hover:underline'>
+      <h2 className='capitalize text-xl font-semibold hover:underline xs:text-lg'>
         {title}
       </h2>
       <FramerImage
@@ -61,7 +61,7 @@ const MovingImg = ({ title, img, link }) => {
         ref={imgRef}
         src={img}
         alt={title}
-        className='z-10 w-96 h-auto hidden absolute rounded-lg'
+        className='z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden'
       />
     </Link>
   );
@@ -73,10 +73,12 @@ const Activity = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
       viewport={{ once: true }}
-      className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border-solid border-dark border-r border-b-4 dark:border-light dark:bg-dark dark:text-light'
+      className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border-solid border-dark border-r border-b-4 dark:border-light dark:bg-dark dark:text-light
+      sm:flex-col
+      '
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
+      <span className='text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm'>{date}</span>
     </motion.li>
   );
 };
@@ -129,9 +131,11 @@ const Certificates = () => {
 
       <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
         <Layout className='pt-16'>
-          <AnimatedText text='Professional Certificates!' className='mb-16' />
+          <AnimatedText text='Professional Certificates!' className='mb-16
+          lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl
+          ' />
 
-          <ul className='grid grid-cols-2 gap-16'>
+          <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-col-1 md:gap-y-16'>
             <FeaturedArticle
               title='Front-End Web Development'
               summary='Completed the Front-End Web Development course at the University of Moratuwa with hands-on experience in assessments and practicals must have strengthened my skills.'
